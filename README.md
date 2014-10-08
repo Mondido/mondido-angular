80e0984231f042338e4f94ff2dcc9224cdb6e145
Robert Falkén <f@lken.se>
2014-10-08 14:53:19 +0200

    readme

--- README.md
+++ README.md
@@ -28,13 +28,13 @@ The controller
     $scope.payment = {};
 
     $scope.payment.amount = '1.00';
-    $scope.payment.paymentRef = 'payref';
+    $scope.payment.paymentRef = '';
     $scope.payment.currency = 'SEK';
-    $scope.payment.hash = 'hash';
+    $scope.payment.hash = '';
     $scope.payment.merchantId = '1';
 
     $scope.mondidoConfig = {
-      url: 'http://api.localmondido.com:3000/v1/transactions',
+      url: 'http://api.mondido.com/v1/transactions',
       success: function(transaction){
         window.alert('success: Transaction '+transaction.id);
         console.log(transaction);
@@ -54,55 +54,4 @@ The controller
 
 The view
 ---
-
-<pre>
-&lt;form payment-form="mondidoConfig"&gt;
-  &lt;div class="form-group"&gt;
-    &lt;label for="card_holder"&gt;Namn på kort&lt;/label&gt;
-    &lt;input type="text" id="card_holder" ng-model="payment.cardHolder" class="form-control" /&gt;
-  &lt;/div&gt;
-  &lt;div class="row"&gt;
-    &lt;div class="form-group col-sm-6"&gt;
-      &lt;label for="card_number"&gt;Kortnummer&lt;/label&gt;
-      &lt;input type="text" id="card_number" ng-model="payment.cardNumber" class="form-control" /&gt;
-    &lt;/div&gt;
-    &lt;div class="form-group col-sm-2"&gt;
-      &lt;label for="card_cvv"&gt;CVV&lt;/label&gt;
-      &lt;input type="text" id="card_cvv" ng-model="payment.cardCvv" class="form-control" /&gt;
-    &lt;/div&gt;
-    &lt;div class="form-group col-sm-2"&gt;
-      &lt;label for="card_expiry"&gt;Utångsdatum&lt;/label&gt;
-      &lt;input type="text" id="card_expiry" ng-model="payment.cardExpiry" class="form-control" /&gt;
-    &lt;/div&gt;
-    &lt;div class="form-group col-sm-2"&gt;
-      &lt;label for="card_type"&gt;Korttyp&lt;/label&gt;
-      &lt;input type="text" id="card_type" ng-model="payment.cardType" class="form-control" /&gt;
-    &lt;/div&gt;
-  &lt;/div&gt;
-  &lt;div class="row"&gt;
-    &lt;div class="form-group col-sm-2"&gt;
-      &lt;label for="amount"&gt;Summa&lt;/label&gt;
-      &lt;input type="text" id="amount" ng-model="payment.amount" class="form-control" /&gt;
-    &lt;/div&gt;
-    &lt;div class="form-group col-sm-2"&gt;
-      &lt;label for="payment_ref"&gt;Paym. Ref&lt;/label&gt;
-      &lt;input type="text" id="payment_ref" ng-model="payment.paymentRef" class="form-control" /&gt;
-    &lt;/div&gt;
-    &lt;div class="form-group col-sm-2"&gt;
-      &lt;label for="currency"&gt;Valuta&lt;/label&gt;
-      &lt;input type="text" id="currency" ng-model="payment.currency" class="form-control" /&gt;
-    &lt;/div&gt;
-    &lt;div class="form-group col-sm-2"&gt;
-      &lt;label for="merchant_id"&gt;Merch&lt;/label&gt;
-      &lt;input type="text" id="merchant_id" ng-model="payment.merchantId" class="form-control" /&gt;
-    &lt;/div&gt;
-    &lt;div class="form-group col-sm-4"&gt;
-      &lt;label for="hash"&gt;Hash&lt;/label&gt;
-      &lt;input type="text" id="hash" ng-model="payment.hash" class="form-control" /&gt;
-    &lt;/div&gt;
-  &lt;/div&gt;
-  &lt;div class="form-group"&gt;
-    &lt;button class="btn btn-primary"&gt;Betala&lt;/button&gt;
-  &lt;/div&gt;
-&lt;/form&gt;
-</pre>
+Coming soon...
--- mondido.directive.js
+++ mondido.directive.js
@@ -1,11 +1,11 @@
 angular.module('mondido', [])
-  .directive('paymentForm', function () {
+  .directive('paymentConfig', function () {
     return {
       restrict: 'A',
       link: function (scope, element, attrs) {
 
 
-    /*Copyright (c) 2009 pidder <www.pidder.com>*/
+    /*Copyright (c) 2009 pidder <www.pidder.comoOcono*/
 
     // pidcrypt
     function pidCrypt(){function a(b){if(!b){b=8}var c=new Array(b);var e=[];for(var d=0;d<256;d++){e[d]=d}for(d=0;d<c.length;d++){c[d]=e[Math.floor(Math.random()*e.length)]}return c}this.setDefaults=function(){this.params.nBits=256;this.params.salt=a(8);this.params.salt=pidCryptUtil.byteArray2String(this.params.salt);this.params.salt=pidCryptUtil.convertToHex(this.params.salt);this.params.blockSize=16;this.params.UTF8=true;this.params.A0_PAD=true};this.debug=true;this.params={};this.params.dataIn="";this.params.dataOut="";this.params.decryptIn="";this.params.decryptOut="";this.params.encryptIn="";this.params.encryptOut="";this.params.key="";this.params.iv="";this.params.clear=true;this.setDefaults();this.errors="";this.warnings="";this.infos="";this.debugMsg="";this.setParams=function(c){if(!c){c={}}for(var b in c){this.params[b]=c[b]}};this.getParams=function(){return this.params};this.getParam=function(b){return this.params[b]||""};this.clearParams=function(){this.params={}};this.getNBits=function(){return this.params.nBits};this.getOutput=function(){return this.params.dataOut};this.setError=function(b){this.error=b};this.appendError=function(b){this.errors+=b;return""};this.getErrors=function(){return this.errors};this.isError=function(){if(this.errors.length>0){return true}return false};this.appendInfo=function(b){this.infos+=b;return""};this.getInfos=function(){return this.infos};this.setDebug=function(b){this.debug=b};this.appendDebug=function(b){this.debugMsg+=b;return""};this.isDebug=function(){return this.debug};this.getAllMessages=function(c){var g={lf:"\n",clr_mes:false,verbose:15};if(!c){c=g}for(var h in g){if(typeof(c[h])=="undefined"){c[h]=g[h]}}var b="";var e="";for(var f in this.params){switch(f){case"encryptOut":e=pidCryptUtil.toByteArray(this.params[f].toString());e=pidCryptUtil.fragment(e.join(),64,c.lf);break;case"key":case"iv":e=pidCryptUtil.formatHex(this.params[f],48);break;default:e=pidCryptUtil.fragment(this.params[f].toString(),64,c.lf)}b+="<p><b>"+f+"</b>:<pre>"+e+"</pre></p>"}if(this.debug){b+="debug: "+this.debug+c.lf}if(this.errors.length>0&&((c.verbose&1)==1)){b+="Errors:"+c.lf+this.errors+c.lf}if(this.warnings.length>0&&((c.verbose&2)==2)){b+="Warnings:"+c.lf+this.warnings+c.lf}if(this.infos.length>0&&((c.verbose&4)==4)){b+="Infos:"+c.lf+this.infos+c.lf}if(this.debug&&((c.verbose&8)==8)){b+="Debug messages:"+c.lf+this.debugMsg+c.lf}if(c.clr_mes){this.errors=this.infos=this.warnings=this.debug=""}return b};this.getRandomBytes=function(b){return a(b)}};
@@ -145,16 +145,18 @@ angular.module('mondido', [])
         function createPayloadFromData(data){
           var payload = {};
           var encrypted = [];
+          var config = scope[attrs.paymentConfig];
+          console.log(data);
 
-          if (typeof scope[attrs.paymentForm].encryptedParameters !== 'undefined' && $.isArray(scope[attrs.paymentForm].encryptedParameters)) {
-            encrypted = scope[attrs.paymentForm].encryptedParameters;
+          if (typeof config.encryptedParameters !== 'undefined' && $.isArray(config.encryptedParameters)) {
+            encrypted = config.encryptedParameters;
           }
 
           for (var prop in data) {
             var camelCase = camelToSnake(prop);
             
             if (encrypted.indexOf(prop) > -1) {
-              payload[camelCase] = string_to_encrypted(data[prop], scope[attrs.paymentForm].publicKey);
+              payload[camelCase] = string_to_encrypted(data[prop], config.publicKey);
             } else {
               payload[camelCase] = data[prop];
             }
@@ -165,9 +167,13 @@ angular.module('mondido', [])
           return payload;
         }
         
+        
         element.bind('submit', function(){
-          var payload = createPayloadFromData(scope.payment);
-          var config = scope[attrs.paymentForm];
+          console.log(scope['payment'], attrs);
+          var payload = createPayloadFromData(scope[attrs.ngmodel]);
+          var config = scope[attrs.paymentConfig];
+
+          console.log(payload, config);
 
           $.ajax({
             type: 'POST',
